c005f12050ec0cd9d2a0e03b9a0e43af02460e12
bugracetinn <70979544+bugracetinn@users.noreply.github.com>
2021-03-19 14:22:54 +0300

    Update degisken_list_tuple_fark.py

--- degisken_list_tuple_fark.py
+++ degisken_list_tuple_fark.py
@@ -1,3 +1,35 @@
-#LİSTELEME YÖNYEMİ YAZIMI 
+#LİSTELEME YÖNYEMİ YAZIMI  
+meyveler = ["elma","portakal","muz"]
+print(meyveler)
 
+#ÇIKTI ['elma', 'portakal', 'muz']
 
+    
+#TUPLE YÖNTEMİ YAZIMI   
+meyveler = ("elma","portakal","muz")
+print(meyveler)
+
+#ÇIKTISI
+('elma', 'portakal', 'muz')
+
+#Listenin elemanlarını değiştirebilirsin. Tuple'ın değiştiremezsin. Haliyle tuple daha az bellek harcar ve listeye göre daha kısa sürede iş yapar. 
+
+#LİSTELEME YÖNTEMİNDE EKLEME
+meyveler = ["elma","portakal","muz"]
+print(meyveler)
+#ÇIKTI: ['elma', 'portakal', 'muz']
+meyveler[0] = "armut"
+print(meyveler)
+#ÇIKTI:['armut', 'portakal', 'muz']
+
+#TUPLE YÖNTEMİ İLE EKLEME
+meyveler = ("elma","portakal","muz")
+print(meyveler)
+#ÇIKTISI 
+('elma', 'portakal', 'muz')
+meyveler[0] = "armut"
+# TypeError                                 Traceback (most recent call last)
+#<ipython-input-77-9f594cadf180> in <module>
+#----> 1 meyveler[0] = "armut"
+
+#TypeError: 'tuple' object does not support item assignment                   ->Şeklinde eror verir.
