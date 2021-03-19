aa07f15557d5ebacf5bc72cdffef4352ea7999bf
bugracetinn <70979544+bugracetinn@users.noreply.github.com>
2021-03-19 14:24:40 +0300

    Update degisken_tipleri_atama.py

--- degisken_tipleri_atama.py
+++ degisken_tipleri_atama.py
@@ -6,10 +6,10 @@ sayi = 10                     1sayi = 10
 print(sayi)                   print(1sayi)
 # çıktısı -> 10               # çıktısı -> EROR
 
-* Değişkenlere isim verirken  BOŞLUK bırakarak  isimler verilmez. (degisken 1 -> hata verir)
-* Değişkenlere isim verirken  "*(/ gibi karakterler verilmez. ( fakat _ bu karakter kullanılabilir)
-* Rezerve edilmiş yazılarla isimlendirme yapılmaz örnek olarak degiskene print ismi verilmez çünkü print rezerve bir komuttur. 
-* Eğer bir değişkene String (harf dizisi) kullanılcaksa "" işaretleri arasına yazılması gerekir.
+#* Değişkenlere isim verirken  BOŞLUK bırakarak  isimler verilmez. (degisken 1 -> hata verir)
+#* Değişkenlere isim verirken  "*(/ gibi karakterler verilmez. ( fakat _ bu karakter kullanılabilir)
+#* Rezerve edilmiş yazılarla isimlendirme yapılmaz örnek olarak degiskene print ismi verilmez çünkü print rezerve bir komuttur. 
+#* Eğer bir değişkene String (harf dizisi) kullanılcaksa "" işaretleri arasına yazılması gerekir.
                        <---- ÖRNEK--->
   a = 5                                                                    a = 5b
 #çıktısı -> 5                                                          #çıktısı-> EROR ( Eğer eror almak istemiyorsanız a = "5b" şeklinde yazmanız gerekir)
@@ -42,9 +42,9 @@ print(sehir)
 print("Şehir: " + sehir)    
 print(a + c)
 
-ÇIKTISI -> İstanbul
-           Şehir:İstanbul
-           12
+#ÇIKTISI -> İstanbul
+ #          Şehir:İstanbul
+ #          12
 
 
 
